0691bf3f4b81215b6941d63b07aa2d94182e68c2
‘huangda2027’ <715745278@qq.com>
2021-11-23 20:14:52 +0800

    计算器完成！

--- js/my.js
+++ js/my.js
@@ -57,3 +57,62 @@ function validateForm() {
     document.getElementsByClassName("data")[10].innerHTML = total1.toFixed(2);
 
 }
+
+
+function mixfunction() {
+    var Sfee = parseFloat(document.forms["myForm"]["fee1"].value);
+    var Gfee = parseFloat(document.forms["myForm"]["fee2"].value);
+    var year = tra(document.forms["myForm"]["year"].value);
+    var Srate = tra(document.forms["myForm"]["rate1"].value);
+    var Grate = tra(document.forms["myForm"]["rate2"].value);
+
+
+    var Srate_month = Srate / 12;
+    var Grate_month = Grate / 12;
+    var month = year * 12;
+    var mixfee = Sfee + Gfee;
+
+    var Spaymonth = parseFloat(Sfee * (Srate_month * Math.pow(1 + Srate_month, month)) / (Math.pow(1 + Srate_month, month) - 1));
+    var Stotal = parseFloat(Spaymonth * month);
+    var Sinterest = parseFloat(Stotal - Sfee);
+
+    var Gpaymonth = parseFloat(Gfee * (Grate_month * Math.pow(1 + Grate_month, month)) / (Math.pow(1 + Grate_month, month) - 1));
+    var Gtotal = parseFloat(Gpaymonth * month);
+    var Ginterest = parseFloat(Gtotal - Gfee);
+
+
+
+    var mixpaymonth = Spaymonth + Gpaymonth;
+    var mixinterest = Sinterest + Ginterest;
+    var mixtotal = Stotal + Gtotal;
+
+
+    var Spaymonth1 = Sfee / month;
+    var Stotal1 = (month + 1) * Sfee * Srate_month / 2 + Sfee;
+    var Sinterest1 = Stotal1 - Sfee;
+    var Slower = Spaymonth1 * Srate_month;
+
+    var Gpaymonth1 = Gfee / month;
+    var Gtotal1 = (month + 1) * Gfee * Grate_month / 2 + Gfee;
+    var Ginterest1 = Gtotal1 - Gfee;
+    var Glower = Gpaymonth1 * Grate_month;
+
+    var mixpaymonth1 = Spaymonth1 + Gpaymonth1;
+    var mixinterest1 = Sinterest1 + Ginterest1;
+    var mixtotal1 = Stotal1 + Gtotal1;
+    var mixlower = Slower + Glower;
+
+    document.getElementsByClassName("data")[0].innerHTML = mixfee.toFixed(0);
+    document.getElementsByClassName("data")[1].innerHTML = month;
+    document.getElementsByClassName("data")[2].innerHTML = mixpaymonth.toFixed(2);
+    document.getElementsByClassName("data")[3].innerHTML = mixinterest.toFixed(2);
+    document.getElementsByClassName("data")[4].innerHTML = mixtotal.toFixed(2);
+
+    document.getElementsByClassName("data")[5].innerHTML = mixfee.toFixed(0);
+    document.getElementsByClassName("data")[6].innerHTML = month;
+    document.getElementsByClassName("data")[7].innerHTML = mixpaymonth1.toFixed(2);
+    document.getElementsByClassName("data")[8].innerHTML = mixlower.toFixed(2);
+    document.getElementsByClassName("data")[9].innerHTML = mixinterest1.toFixed(2);
+    document.getElementsByClassName("data")[10].innerHTML = mixtotal1.toFixed(2);
+
+}
